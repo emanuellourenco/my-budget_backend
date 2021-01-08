fd1ee4fd8131814161f078bb04e905b25fd45102
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-08 11:57:33 +0000

    first commit

---
+++ database/migrations/2021_01_08_112452_create_transactions_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions_tags', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->foreign('transaction_id')->references('id')->on('transactions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions_tags');
+    }
+}
