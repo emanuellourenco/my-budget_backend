6904da60a32cbde6668c82eee13e01f8ab07ca20
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-08 15:39:54 +0000

    change api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagsController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/user', UsersController::class);
+Route::apiResource('/tags', TagsController::class);
