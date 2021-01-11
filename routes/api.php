bf1a2c3e2084e1082c8761d73367796bfa2251f0
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-11 12:08:52 +0000

    add auth api routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagsController;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,9 @@ use App\Http\Controllers\UsersController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/signup',[AuthController::class, 'signup']);
+Route::post('/logout',[AuthController::class, 'logout']);
+
 Route::apiResource('/user', UsersController::class);
 Route::apiResource('/tags', TagsController::class);
