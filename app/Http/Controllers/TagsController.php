30b8b9ee42657d977bc5c17f50da42733c53be09
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-11 12:10:44 +0000

    add/change functions in Tags controller

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tag;
+use App\Models\User;
 
 class TagsController extends Controller
 {
@@ -14,7 +15,10 @@ class TagsController extends Controller
      */
     public function index(Request $request)
     {
-        $tags = Tag::where("user_id", $request->user_id)->get();
+        $user = new User;
+        $user_by_token = $user->getUserByToken($request->token);
+
+        $tags = !!$user_by_token ? Tag::where("user_id", $user_by_token->id)->select("name", "color", "id as key", "rule")->get() : [];
 
         return ["tags" => $tags];
     }
@@ -38,14 +42,21 @@ class TagsController extends Controller
     public function store(Request $request)
     {        
         try {
-            $new_tag = new Tag;
-            $new_tag->name = $request->name;
-            $new_tag->color = $request->color;
-            $new_tag->rule = $request->rule;
-            $new_tag->user_id = $request->user_id;
-            $new_tag->save();
+            $user = new User;
+            $user_by_token = $user->getUserByToken($request->token);
 
-            return "tag created";
+            if($user_by_token){
+                $new_tag = new Tag;
+                $new_tag->name = $request->name;
+                $new_tag->color = $request->color;
+                $new_tag->rule = $request->rule;
+                $new_tag->user_id = $user_by_token->id;
+                $new_tag->save();
+    
+                $tags = Tag::where("user_id", $user_by_token->id)->select("name", "color", "id as key", "rule")->get();
+    
+                return ["status"=> "success", "tags" => $tags];
+            }
 
         } catch (Exception $e) {
     
@@ -57,11 +68,27 @@ class TagsController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id,Request $request)
     {
-        //
+        try {
+            $user = new User;
+            $user_by_token = $user->getUserByToken($request->token);
+
+            if($user_by_token){
+                $tag = Tag::where("user_id", $user_by_token->id)->where("id", $id)->first();
+
+                return ["status"=> "success", "tag" => $tag];
+            }
+            
+            return ["status"=> "error", "tag" => [], "user_by_token" => $user_by_token, $id, $request];
+
+        } catch (Exception $e) {
+    
+            return $e;
+        }
     }
 
     /**
@@ -85,18 +112,25 @@ class TagsController extends Controller
     public function update(Request $request, $id)
     {
         try {
-            $update_tag = Tag::findOrFail($id);
+            $user = new User;
+            $user_by_token = $user->getUserByToken($request->token);
 
-            if($update_tag){
-                $update_tag->name = $request->name;
-                $update_tag->color = $request->color;
-                $update_tag->rule = $request->rule;
-                $update_tag->save();
+            if($user_by_token){
+                $update_tag = Tag::where("user_id", $user_by_token->id)->where("id",$id)->first();
 
-                return "tag updated";
+                if($update_tag){
+                    $update_tag->name = $request->name;
+                    $update_tag->color = $request->color;
+                    $update_tag->rule = $request->rule;
+                    $update_tag->save();
 
-            } else {
-                return "tag not found";
+                    $tags = Tag::where("user_id", $user_by_token->id)->select("name", "color", "id as key", "rule")->get();
+
+                    return ["status"=> "success", "tags" => $tags];
+
+                } else {
+                    return "tag not found";
+                }
             }
 
         } catch (Exception $e) {
