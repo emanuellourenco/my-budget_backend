5a7e919e4bf758e10664b09e7494e1a854f51380
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-21 17:43:21 +0000

    add tags options and transactions routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagsController;
+use App\Http\Controllers\TransactionsController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\AuthController;
 
@@ -16,9 +17,11 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/login',[AuthController::class, 'login']);
-Route::post('/signup',[AuthController::class, 'signup']);
-Route::post('/logout',[AuthController::class, 'logout']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::apiResource('/user', UsersController::class);
 Route::apiResource('/tags', TagsController::class);
+Route::post('/tags/options', [TagsController::class, 'options']);
+Route::apiResource('/transactions', TransactionsController::class);
