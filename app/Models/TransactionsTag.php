db2f329234e5c00e960eb027f464e675684b93d4
Emanuel Lourenço <dodgy6@gmail.com>
2021-01-21 17:42:08 +0000

    rename model from Transaction_Tag to TransactionsTag

--- app/Models/TransactionsTag.php
+++ app/Models/TransactionsTag.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Transaction_Tag extends Model
+class TransactionsTag extends Model
 {
     use HasFactory;
 
