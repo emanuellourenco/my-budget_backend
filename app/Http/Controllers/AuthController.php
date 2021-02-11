d4478c7fecfa6c94decae6b89b0a6c2e46c0062f
Emanuel Lourenço <dodgy6@gmail.com>
2021-02-11 12:45:22 +0000

    send login/signup errors in api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 {
     /**
      * Login user.
-     * 
+     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -20,23 +20,34 @@ class AuthController extends Controller
         $email = $request->email;
         $password = $request->password;
 
-        $user = User::where("email", $email)->first();
-        if($user){
+        $user = User::where('email', $email)->first();
+        if ($user) {
+            $is_auth = Hash::check($password, $user->password);
 
-            $is_auth = Hash::check($password,$user->password);
-    
-            if($is_auth){
-                $token = hash('sha256',Str::random(60));
+            if ($is_auth) {
+                $token = hash('sha256', Str::random(60));
                 $user->remember_token = $token;
                 $user->save();
-    
-                return ["status" => "success", "message"=>"Login successfuly", "token" => $token];
+
+                return [
+                    'status' => 'success',
+                    'message' => 'Login successfuly',
+                    'token' => $token,
+                ];
             }
-            
-            return ["status" => "error", "message"=>"Email or password don´t match", "token" => null];
-        } 
-        
-        return ["status" => "error", "message"=>"Email or password don´t match", "token" => null];
+
+            return [
+                'status' => 'error',
+                'message' => 'Email or password don´t match',
+                'token' => null,
+            ];
+        }
+
+        return [
+            'status' => 'error',
+            'message' => 'Email or password don´t match',
+            'token' => null,
+        ];
     }
 
     /**
@@ -47,11 +58,29 @@ class AuthController extends Controller
      */
     public function logout($token)
     {
-        $user = User::where("remember_token", $token)->first();
+        $user = User::where('remember_token', $token)->first();
         $user->remember_token = null;
         $user->save();
 
-        return ["status" => "success", "message" => ""];
+        return ['status' => 'success', 'message' => ''];
+    }
+
+    function checkRequiredFields($request, $field_list)
+    {
+        $errors = [];
+        foreach ($field_list as &$field) {
+            if (empty($request->input($field))) {
+                array_push(
+                    $errors,
+                    (object) [
+                        'field' => $field,
+                        'message' => 'Name is required!',
+                    ]
+                );
+            }
+        }
+
+        return $errors;
     }
 
     /**
@@ -62,28 +91,49 @@ class AuthController extends Controller
      */
     public function signup(Request $request)
     {
+        $field_list = ['name', 'email', 'password', 'password2'];
+        $errors = $this->checkRequiredFields($request, $field_list);
+        $status = 'error';
         $email = $request->email;
-        $find_user = User::where("email", $email)->first();
-
-        if($find_user){
-            return ["status"=> "error", "message"=>"Email already exists"];
-        }else{
-
+        // Check if have any user register with the same email
+        $find_user = User::where('email', $email)->first();
+        if ($find_user) {
+            array_push(
+                $errors,
+                (object) [
+                    'field' => 'email',
+                    'message' => 'Email already exists',
+                ]
+            );
+        }
+        // Check if both passwords match
+        $password = $request->password;
+        $password2 = $request->password2;
+        if ($password !== $password2) {
+            array_push(
+                $errors,
+                (object) [
+                    'field' => 'password2',
+                    'message' => "Passwords don't match",
+                ]
+            );
+        }
+        // Create user if don't have find any errors
+        if (!count($errors)) {
             $password = $request->password;
             $password2 = $request->password2;
-    
-            if($password === $password2){
-    
-                $new_user = new User;
+
+            if ($password === $password2) {
+                $status = 'success';
+
+                $new_user = new User();
                 $new_user->name = $request->name;
                 $new_user->email = $email;
                 $new_user->password = Hash::make($password);
                 $new_user->save();
-    
-                return ["status"=> "success", "message"=>"User created"];
-            }else{
-                return ["status"=> "error", "message"=>"Passwords don't match"];
             }
         }
+
+        return ['status' => $status, 'errors' => $errors];
     }
 }
