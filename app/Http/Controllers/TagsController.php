e0dacb9519a523b55bb43a5b2179d8a2f763ade1
Emanuel Lourenço <dodgy6@gmail.com>
2021-03-30 19:39:09 +0100

    app/Http/Controllers/TagsController.php
    refactor TagsController functions

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -2,136 +2,110 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Tag;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class TagsController extends Controller
 {
-    /**
-     * Function to list tags and return an array with tags list and tags count.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
+    public function getList($request, $fields)
     {
         $user = new User();
         $user_by_token = $user->getUserByToken($request->token);
+        $tags = [];
+        $total_count = 0;
 
         if ($user_by_token) {
-            $offset = !!$request->offset ? $request->offset : 0;
+            $offset = (bool) $request->offset ? $request->offset : 0;
             $limit = 100;
-            if (!!$request->limit && $request->limit < 100) {
+            if ((bool) $request->limit && $request->limit < 100) {
                 $limit = $request->limit;
             }
-            $order_by = !!$request->orderBy ? $request->orderBy : 'name';
-            $sort_by = !!$request->sortBy ? $request->sortBy : 'asc';
+            $order_by = (bool) $request->orderBy ? $request->orderBy : 'name';
+            $sort_by = (bool) $request->sortBy ? $request->sortBy : 'asc';
 
             $tags = Tag::where('user_id', $user_by_token->id)
-                ->select('name', 'color', 'id as key', 'rule')
+                ->select($fields)
                 ->orderBy($order_by, $sort_by)
                 ->skip($offset)
                 ->take($limit)
                 ->get();
 
             $total_count = Tag::where('user_id', $user_by_token->id)->count();
-
-            return [
-                'status' => 200,
-                'tags' => $tags,
-                'total_count' => $total_count,
-            ];
         }
 
-        return ['status' => 401, 'tags' => [], 'total_count' => 0];
+        return ['tags' => $tags, 'total_count' => $total_count];
+    }
+
+    /**
+     * Function to list tags and return an array with tags list and tags count.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $fields = ['name', 'color', 'id as key', 'rule'];
+        $list = $this->getList($request, $fields);
+        $tags = $list['tags'];
+        $total_count = $list['total_count'];
+
+        return ['tags' => $tags, 'total_count' => $total_count];
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function options(Request $request)
     {
-        $user = new User();
-        $user_by_token = $user->getUserByToken($request->token);
+        $fields = ['name as label', 'id as value'];
+        $list = $this->getList($request, $fields);
+        $tags = $list['tags'];
+        $total_count = $list['total_count'];
 
-        if ($user_by_token) {
-            $offset = !!$request->offset ? $request->offset : 0;
-            $limit = 100;
-            if (!!$request->limit && $request->limit < 100) {
-                $limit = $request->limit;
+        return ['tags' => $tags, 'total_count' => $total_count];
+    }
+
+    public function checkFields($request, $required)
+    {
+        $error = false;
+        $data_error = [];
+
+        foreach ($required as &$field) {
+            $validateField = $request->input($field);
+
+            if (!$validateField) {
+                $error = true;
+
+                array_push($data_error, [
+                    'field' => $field,
+                    'label' => 'This field is required',
+                ]);
             }
-            $order_by = !!$request->orderBy ? $request->orderBy : 'name';
-
-            $tags = Tag::where('user_id', $user_by_token->id)
-                ->select('name as label', 'id as value')
-                ->orderBy($order_by, 'asc')
-                ->skip($offset)
-                ->take($limit)
-                ->get();
-
-            $total_count = Tag::where('user_id', $user_by_token->id)->count();
-
-            return [
-                'status' => 200,
-                'tags' => $tags,
-                'total_count' => $total_count,
-            ];
         }
 
-        return ['status' => 401, 'tags' => [], 'total_count' => 0];
+        return ['error' => $error, 'data_error' => $data_error];
     }
 
     /**
      * Store a newly created tag.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         try {
-            $data_error = [];
-            $error = false;
-            $name = $request->name;
-            $color = $request->color;
-            $rule = $request->rule;
-
-            if (!$name) {
-                array_push($data_error, [
-                    'field' => 'name',
-                    'label' => 'This field is required',
-                ]);
-            }
-            if (!$color) {
-                array_push($data_error, [
-                    'field' => 'color',
-                    'label' => 'This field is required',
-                ]);
-            }
-            if (!$rule) {
-                array_push($data_error, [
-                    'field' => 'rule',
-                    'label' => 'This field is required',
-                ]);
-            }
-            if (!$name || !$color || !$rule) {
-                $error = true;
-            }
+            $requiredFields = ['name', 'color', 'rule'];
+            $validateFields = $this->checkFields($request, $requiredFields);
+            $data_error = $validateFields['data_error'];
+            $error = $validateFields['error'];
+            $tags = [];
+            $total_count = 0;
 
             if (!$error) {
                 $user = new User();
                 $user_by_token = $user->getUserByToken($request->token);
-                $offset = !!$request->offset ? $request->offset : 0;
-                $limit = 100;
-                if (!!$request->limit && $request->limit < 100) {
-                    $limit = $request->limit;
-                }
-                $order_by = !!$request->orderBy ? $request->orderBy : 'name';
-                $sort_by = !!$request->sortBy ? $request->sortBy : 'asc';
 
                 if ($user_by_token) {
                     $new_tag = new Tag();
@@ -141,29 +115,15 @@ class TagsController extends Controller
                     $new_tag->user_id = $user_by_token->id;
                     $new_tag->save();
 
-                    $tags = Tag::where('user_id', $user_by_token->id)
-                        ->select('name', 'color', 'id as key', 'rule')
-                        ->orderBy($order_by, $sort_by)
-                        ->skip($offset)
-                        ->take($limit)
-                        ->get();
-
-                    $total_count = Tag::where(
-                        'user_id',
-                        $user_by_token->id
-                    )->count();
-
-                    return [
-                        'status' => 201,
-                        'tags' => $tags,
-                        'total_count' => $total_count,
-                    ];
+                    // Get new tags list based on offset and table limit
+                    $fields = ['name', 'color', 'id as key', 'rule'];
+                    $list = $this->getList($request, $fields);
+                    $tags = $list['tags'];
+                    $total_count = $list['total_count'];
                 }
-
-                return ['status' => 401, 'tags' => [], 'total_count' => 0];
             }
 
-            return ['status' => 401, 'error' => $data_error];
+            return ['tags' => $tags, 'total_count' => $total_count, 'error' => $data_error];
         } catch (Exception $e) {
             return $e;
         }
@@ -172,8 +132,8 @@ class TagsController extends Controller
     /**
      * Get the specified tag to update.
      *
-     * @param  int  $id
-     * @param  \Illuminate\Http\Request  $request
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id, Request $request)
@@ -181,16 +141,15 @@ class TagsController extends Controller
         try {
             $user = new User();
             $user_by_token = $user->getUserByToken($request->token);
+            $tag = [];
 
             if ($user_by_token) {
                 $tag = Tag::where('user_id', $user_by_token->id)
                     ->where('id', $id)
                     ->first();
-
-                return ['status' => 200, 'tag' => $tag];
             }
 
-            return ['status' => 401, 'tag' => []];
+            return ['tag' => $tag];
         } catch (Exception $e) {
             return $e;
         }
@@ -199,62 +158,34 @@ class TagsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
     }
 
     /**
      * Update the specified tag.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
         try {
-            $data_error = [];
-            $error = false;
-            $name = $request->name;
-            $color = $request->color;
-            $rule = $request->rule;
-
-            if (!$name) {
-                array_push($data_error, [
-                    'field' => 'name',
-                    'label' => 'This field is required',
-                ]);
-            }
-            if (!$color) {
-                array_push($data_error, [
-                    'field' => 'color',
-                    'label' => 'This field is required',
-                ]);
-            }
-            if (!$rule) {
-                array_push($data_error, [
-                    'field' => 'rule',
-                    'label' => 'This field is required',
-                ]);
-            }
-            if (!$name || !$color || !$rule) {
-                $error = true;
-            }
+            $requiredFields = ['name', 'color', 'rule'];
+            $validateFields = $this->checkFields($request, $requiredFields);
+            $data_error = $validateFields['data_error'];
+            $error = $validateFields['error'];
+            $tags = [];
+            $total_count = 0;
 
             if (!$error) {
                 $user = new User();
                 $user_by_token = $user->getUserByToken($request->token);
-                $offset = !!$request->offset ? $request->offset : 0;
-                $limit = 100;
-                if (!!$request->limit && $request->limit < 100) {
-                    $limit = $request->limit;
-                }
-                $order_by = !!$request->orderBy ? $request->orderBy : 'name';
-                $sort_by = !!$request->sortBy ? $request->sortBy : 'asc';
 
                 if ($user_by_token) {
                     $update_tag = Tag::where('user_id', $user_by_token->id)
@@ -267,32 +198,16 @@ class TagsController extends Controller
                         $update_tag->rule = $request->rule;
                         $update_tag->save();
 
-                        $tags = Tag::where('user_id', $user_by_token->id)
-                            ->select('name', 'color', 'id as key', 'rule')
-                            ->orderBy($order_by, $sort_by)
-                            ->skip($offset)
-                            ->take($limit)
-                            ->get();
-
-                        $total_count = Tag::where(
-                            'user_id',
-                            $user_by_token->id
-                        )->count();
-
-                        return [
-                            'status' => 200,
-                            'tags' => $tags,
-                            'total_count' => $total_count,
-                        ];
+                        // Get new tags list based on offset and table limit
+                        $fields = ['name', 'color', 'id as key', 'rule'];
+                        $list = $this->getList($request, $fields);
+                        $tags = $list['tags'];
+                        $total_count = $list['total_count'];
                     }
-
-                    return ['status' => 404, 'tags' => [], 'total_count' => 0];
                 }
-
-                return ['status' => 401, 'tags' => [], 'total_count' => 0];
             }
 
-            return ['status' => 401, 'error' => $data_error];
+            return ['error' => $data_error, 'tags' => $tags, 'total_count' => $total_count];
         } catch (Exception $e) {
             return $e;
         }
@@ -301,8 +216,8 @@ class TagsController extends Controller
     /**
      * Remove the specified tag.
      *
-     * @param  int  $id
-     * @param  \Illuminate\Http\Request  $request
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id, Request $request)
@@ -310,13 +225,8 @@ class TagsController extends Controller
         try {
             $user = new User();
             $user_by_token = $user->getUserByToken($request->token);
-            $offset = !!$request->offset ? $request->offset : 0;
-            $limit = 100;
-            if (!!$request->limit && $request->limit < 100) {
-                $limit = $request->limit;
-            }
-            $order_by = !!$request->orderBy ? $request->orderBy : 'name';
-            $sort_by = !!$request->sortBy ? $request->sortBy : 'asc';
+            $tags = [];
+            $total_count = 0;
 
             if ($user_by_token) {
                 $delete_tag = Tag::where('user_id', $user_by_token->id)
@@ -326,29 +236,15 @@ class TagsController extends Controller
                 if ($delete_tag) {
                     $delete_tag->delete();
 
-                    $tags = Tag::where('user_id', $user_by_token->id)
-                        ->select('name', 'color', 'id as key', 'rule')
-                        ->orderBy($order_by, $sort_by)
-                        ->skip($offset)
-                        ->take($limit)
-                        ->get();
-
-                    $total_count = Tag::where(
-                        'user_id',
-                        $user_by_token->id
-                    )->count();
-
-                    return [
-                        'status' => 200,
-                        'tags' => $tags,
-                        'total_count' => $total_count,
-                    ];
+                    // Get new tags list based on offset and table limit
+                    $fields = ['name', 'color', 'id as key', 'rule'];
+                    $list = $this->getList($request, $fields);
+                    $tags = $list['tags'];
+                    $total_count = $list['total_count'];
                 }
-
-                return ['status' => 404, 'tags' => [], 'total_count' => 0];
             }
 
-            return ['status' => 401, 'tags' => [], 'total_count' => 0];
+            return ['tags' => $tags, 'total_count' => $total_count];
         } catch (Exception $e) {
             return $e;
         }
